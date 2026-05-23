296e8ef59555aa9963b8fd8909d3d7a53b8a2cb2
Smooth Cube <backend@smoothcube.net>
2026-05-23 22:29:16 +0100

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,19 @@
-import subprocess
-import sys
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+from main import get_bot
+from dash import app
+
+async def main():
+    bot, token = get_bot()
+
+    hypercorn_config = Config()
+    hypercorn_config.bind = ["0.0.0.0:8080"]
+
+    await asyncio.gather(
+        bot.start(token),
+        serve(app, hypercorn_config),
+    )
 
 if __name__ == "__main__":
-    subprocess.run([sys.executable, "main.py"], check=True)
+    asyncio.run(main())
