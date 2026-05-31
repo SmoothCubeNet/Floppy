fa62ad62506fa50660bb905a3a8add4f134b8d45
Smooth Cube <backend@smoothcube.net>
2026-06-01 00:57:26 +0100

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -2,17 +2,23 @@ import asyncio
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 from main import get_bot
-from dash import app
+from dash import app as dash_app
+from messenger import messenger_app
+
 
 async def main():
     bot, token = get_bot()
 
-    hypercorn_config = Config()
-    hypercorn_config.bind = ["0.0.0.0:8080"]
+    dash_config = Config()
+    dash_config.bind = ["0.0.0.0:8080"]
+
+    messenger_config = Config()
+    messenger_config.bind = ["0.0.0.0:8081"]
 
     await asyncio.gather(
         bot.start(token),
-        serve(app, hypercorn_config),
+        serve(dash_app, dash_config),
+        serve(messenger_app, messenger_config),
     )
 
 if __name__ == "__main__":
